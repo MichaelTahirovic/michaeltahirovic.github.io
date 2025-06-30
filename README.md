90026c8a3d6364da9ec08d5438bd6ad44abf2a7c
Michael Tahirovic <163599301+MichaelTahirovic@users.noreply.github.com>
2025-06-30 13:00:48 -0400

    Reorganized React app

--- README.md
+++
@@ -1,2 +0,0 @@
-# My Website!!!
-These are the files to my very own personal website. I'm planning on building it up over time to add in all my cool projects and intrests but right now it's probably not looking like much.
--- michaels-portfolio/src/App.js
+++ michaels-portfolio/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer'; // If you have a Footer component
+import About from './pages/About';
+import Home from './pages/Home';
+import Jobs from './pages/Jobs';
+import Projects from './pages/Projects';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          {<Route path="/" element={<Home />} />}
+          <Route path="/about" element={<About />} />
+          {<Route path="/jobs" element={<Jobs />} />}
+          {<Route path="/projects" element={<Projects />} />}
+        </Routes>
+      </main>
+      {<Footer />}
+    </Router>
   );
 }
 
